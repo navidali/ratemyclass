a24c4afe6058e44bfb979f2cce891d619ca5a17b
Navid Ali <43358902+navidali@users.noreply.github.com>
2022-11-09 02:49:31 -0500

    fixed bugs with carousel

--- models/reviews.js
+++ models/reviews.js
@@ -4,5 +4,6 @@ var reviewSchema = new mongoose.Schema({
   course_name: String,
   rating: Number,
   description: String,
+  date: { type: Date, default: Date.now },
 });
 module.exports = mongoose.model("Review", reviewSchema);
